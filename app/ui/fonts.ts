38c1ec97c3fb60514220bdee855a5f1a9a566224
summuva <joseaveiro2001@gmail.com>
2025-02-28 16:54:54 -0300

    update lusitana font configuration to include subsets; this cause error in deploy

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,4 @@
 import { Inter } from 'next/font/google';
 import { Lusitana } from 'next/font/google';
 export const inter = Inter({ subsets: ['latin'] });
-export const lusitana = Lusitana({ weight: '400' }); 
+export const lusitana = Lusitana({ weight: '400', subsets: ['latin'] }); 
